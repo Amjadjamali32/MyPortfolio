433c3e95392463b6aa5cb7572ecaae5092c57612
Amjad Ali Jamali <amjadalijamali41@gmail.com>
2024-12-28 12:17:38 +0500

    Added Projects

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,9 @@
 import portfolioImage from "./assets/portfolio.png"
 import weatherAppImage from "./assets/weatherApp.png"
 import HealthcareAssistantImage from "./assets/assistant.png"
+import blogifyImage from "./assets/blogify.png"
+import taskManagerImage from "./assets/task_manager.png"
+import ecommerceStoreImage from "./assets/aj.png"
 
 export const projectsData = [
     {
@@ -26,6 +29,29 @@ export const projectsData = [
         "skills": ["React", "Express", "Node"],
         "demo": "/",
         "source": "https://github.com/Amjadjamali32/AI-Healthcare-Assistant.git"
+    },
+    {
+        "title": "Blogify",
+        "imageSrc": blogifyImage,
+        "description": "Blogify is a blog display application it is just a front-end application. It is a simple blog display application where you can see the blogs and read them.",
+        "skills": ["React", "tailwindcss", "JavaScript"],
+        "demo": "https://blogify-21sw18.netlify.app/",
+        "source": "https://github.com/Amjadjamali32/Blogify.git"
+    },
+    {
+        "title": "Ecommerce Store",
+        "imageSrc": ecommerceStoreImage,
+        "description": "AJ Store is an e-commerce store where you can buy products. It is a front-end application. It is a simple e-commerce store where you can see the products and buy them.It has also cart functionality.",
+        "skills": ["React", "tailwindcss", "Responsive Design"],    
+        "demo": "https://jamali4u.netlify.app/",
+        "source": "https://github.com/Amjadjamali32/Ecommerce-Store.git"
+    },
+    {
+        "title": "Advanced Task Manager",
+        "imageSrc": taskManagerImage,
+        "description": "It is an Advanced task manager where you can add, delete, and update the tasks. It is a front-end application. It is has categories feature where you can categories your tasks.",
+        "skills": ["React", "tailwindcss", "Fr"],
+        "demo": "https://task-manager-21sw18.netlify.app/",
+        "source": "https://github.com/Amjadjamali32/Task-Manger.git"
     }
 ]
-
